c6363aab2f26aa4fac7a8bb7572216803706f338
Fiona Mellink <fimellink@gmail.com>
2024-01-03 07:42:01 +1100

    Lab for review

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+function submitData (userName, userEmail) {
+    return fetch("http://localhost:3000/users", 
+    {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: userEmail,
+        }),
+      })
+      .then((response) =>
+        response.json()
+      )
+      .then((user) => {
+        const userId = document.createElement("p");
+        userId.textContent = user.id;
+        document.body.appendChild(userId);
+      })
+      .catch((error) => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage)
+      });
+};
